bd986c49a4fe449493760d56d9f747a1781631a3
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2017-07-09 12:32:07 -0300

    Adding a long docstring to the ExecTrace class.

--- exec_trace.py
+++ exec_trace.py
@@ -29,6 +29,49 @@ VERBOSE = 1 # informative non-error msgs to the user
 DEBUG = 2   # debugging messages to the developer
 
 class ExecTrace():
+  """ ExecTrace is a generic class that implements an
+      algorithm for mapping all reachable code-paths
+      in a given binary.
+
+      As a sub-product it can also emit:
+      (1) a disassembly listing
+      (2) a flow-chart
+      (3) a map of code-regions versus data-regions
+
+      This class must be inherited by a child-class
+      providing a disasm_instruction method which
+      (a) uses self.fetch() to read consecutive bytes from
+      code memory
+      (b) returns a string representing the disassembly of
+      the current instruction
+      (c) invokes the class methods listed below to declare
+      the behaviour of the branching instructions.
+
+      Instruction description methods:
+        * subroutine(address)
+           Declares that the current instruction
+           invokes a subroutine at <address>
+
+        * return_from_subroutine()
+           Declares that the current instruction
+           terminates the execution of a subroutine
+           and jumps back to the code that originally
+           invoked the subroutine.
+
+        * conditional_branch(address)
+           Declares that the current instruction
+           is a conditional branch that may
+           jump to <address>
+
+        * unconditional_jump(address)
+           Declares that the current instruction
+           is an unconditional jump to <address>
+
+        * illegal_instruction(opcode)
+           Declares that the current instruction
+           with operation code <opcode> could not
+           be parsed as a valid known instruction.
+  """
   def __init__(self, romfile, loglevel=ERROR):
     self.loglevel = loglevel
     self.rom = open(romfile).read()
@@ -38,59 +81,18 @@ class ExecTrace():
     self.PC = None
     self.disasm = {}
 
-  def already_visited(self, address):
-    if self.PC is not None:
-      if address >= self.current_entry_point and address < self.PC:
-        self.log(DEBUG, "RECENTLY: (PC={} address={})".format(hex(self.PC), hex(address)))
-        return True
+### Public method to start the binary code interpretation ###
+  def run(self, entry_point=0x0000):
+    self.current_entry_point = entry_point
+    self.PC = entry_point
+    self.disasm[entry_point] = ""
+    self.indent = "%04X: " % entry_point
+    while self.PC is not None:
+      line = self.disasm_instruction()
+      self.disasm[self.current_entry_point] += "%s%s\n" % (self.indent, line)
+      self.indent = "      "
 
-    for codeblock in self.visited_ranges:
-      if address >= codeblock.start and address <= codeblock.end:
-        self.log(DEBUG, "ALREADY VISITED: {}".format(hex(address)))
-        if address > codeblock.start:
-          # split the block into two:
-          new_block = CodeBlock(start=codeblock.start,
-                                end=address-1,
-                                next_block=[address])
-          codeblock.start = address
-          # and also split ownership of subroutine calls:
-          for instr_addr, call_addr in codeblock.subroutines.iteritems():
-            if instr_addr < address:
-              new_block.add_subroutine_call(instr_addr, call_addr)
-              del codeblock.subroutines[instr_addr]
-          self.visited_ranges.append(new_block)
-        return True
-
-    # otherwise:
-    return False
-
-  def restart_from_another_entry_point(self):
-    if len(self.pending_entry_points) == 0:
-      self.PC = None  # This will finish the crawling
-    else:
-      address = self.pending_entry_points.pop()
-      self.current_entry_point = address
-      self.PC = address
-      self.log(VERBOSE, "Restarting from: {}".format(hex(address)))
-
-  def add_range(self, start, end, exit=None):
-    if end < start:
-      self.add_range(end, start, exit)
-      return
-
-    self.log(DEBUG, "=== New Range: start: {}  end: {} ===".format(hex(start), hex(end)))
-    block = CodeBlock(start, end, exit)
-    self.visited_ranges.append(block)
-
-  def schedule_entry_point(self, address):
-    if self.already_visited(address):
-      return
-
-    if address not in self.pending_entry_points:
-      self.pending_entry_points.append(address)
-      self.log(VERBOSE, "SCHEDULING: {}".format(hex(address)))
-      self.log_status()
-
+### Methods for declaring the behaviour of branching instructions ###
   def subroutine(self, address):
     self.add_range(start=self.current_entry_point,
                    end=self.PC-1,
@@ -147,6 +149,62 @@ class ExecTrace():
     self.log(ERROR, "[{}] ILLEGAL: {}".format(hex(self.PC-1), hex(opcode)))
     self.restart_from_another_entry_point()
 
+### Private methods for computing the code-execution graph structure ###
+  def already_visited(self, address):
+    if self.PC is not None:
+      if address >= self.current_entry_point and address < self.PC:
+        self.log(DEBUG, "RECENTLY: (PC={} address={})".format(hex(self.PC), hex(address)))
+        return True
+
+    for codeblock in self.visited_ranges:
+      if address >= codeblock.start and address <= codeblock.end:
+        self.log(DEBUG, "ALREADY VISITED: {}".format(hex(address)))
+        if address > codeblock.start:
+          # split the block into two:
+          new_block = CodeBlock(start=codeblock.start,
+                                end=address-1,
+                                next_block=[address])
+          codeblock.start = address
+          # and also split ownership of subroutine calls:
+          for instr_addr, call_addr in codeblock.subroutines.iteritems():
+            if instr_addr < address:
+              new_block.add_subroutine_call(instr_addr, call_addr)
+              del codeblock.subroutines[instr_addr]
+          self.visited_ranges.append(new_block)
+        return True
+
+    # otherwise:
+    return False
+
+  def restart_from_another_entry_point(self):
+    if len(self.pending_entry_points) == 0:
+      self.PC = None  # This will finish the crawling
+    else:
+      address = self.pending_entry_points.pop()
+      self.disasm[address] = ""
+      self.indent = "%04X: " % address
+      self.current_entry_point = address
+      self.PC = address
+      self.log(VERBOSE, "Restarting from: {}".format(hex(address)))
+
+  def add_range(self, start, end, exit=None):
+    if end < start:
+      self.add_range(end, start, exit)
+      return
+
+    self.log(DEBUG, "=== New Range: start: {}  end: {} ===".format(hex(start), hex(end)))
+    block = CodeBlock(start, end, exit)
+    self.visited_ranges.append(block)
+
+  def schedule_entry_point(self, address):
+    if self.already_visited(address):
+      return
+
+    if address not in self.pending_entry_points:
+      self.pending_entry_points.append(address)
+      self.log(VERBOSE, "SCHEDULING: {}".format(hex(address)))
+      self.log_status()
+
   def increment_PC(self):
     if self.already_visited(self.PC):
       self.log(VERBOSE, "ALREADY BEEN AT {}!".format(hex(self.PC)))
@@ -163,16 +221,6 @@ class ExecTrace():
     self.log(DEBUG, "Fetch at {}: {}".format(hex(self.PC), hex(value)))
     self.increment_PC()
     return value
-
-  def run(self, entry_point=0x0000):
-    self.current_entry_point = entry_point
-    self.PC = entry_point
-    self.disasm[entry_point] = ""
-    indent = "%04X: " % entry_point
-    while self.PC is not None:
-      line = self.disasm_instruction()
-      self.disasm[entry_point] += "%s%s\n" % (indent, line)
-      indent = "      "
 
 ####### LOGGING #######
   def log(self, loglevel, msg):
@@ -219,8 +267,11 @@ class ExecTrace():
 
   def save_disassembly_listing(self, filename="output.asm"):
     asm = open(filename, "w")
-    for codeblock in self.get_grouped_ranges():
-      asm.write(self.disasm[codeblock[0]])
+    for codeblock in sorted(self.visited_ranges, key=lambda cb: cb.start):
+      if codeblock.start in self.disasm:
+        asm.write(self.disasm[codeblock.start])
+      else:
+        asm.write("// MISSING: %04X\n" % codeblock.start)
     asm.close()
 
 def generate_graph():
