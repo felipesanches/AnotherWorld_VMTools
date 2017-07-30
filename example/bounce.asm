f61e195e472a3f9d33cb4c3d1e694df4f5a0a9a3
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2017-07-30 01:08:34 -0300

    use 2's complement for representing negative speeds on bounce.asm

--- example/bounce.asm
+++ example/bounce.asm
@@ -18,14 +18,8 @@ init:
       mov [BALL_Y], 100
       mov [BALL_ZOOM], 0x40
 
-; The VX and VY vars are strictly positive values, so we treat
-; zero as -SPEED,
-; SPEED as zero
-; 2*SPEED as +SPEED
       mov [BALL_VX], SPEED
-      add [BALL_VX], SPEED
       mov [BALL_VY], SPEED
-      add [BALL_VY], SPEED
       setPalette 0x03 ; This is one the color palette used for that code wheel symbol.
       selectVideoPage 0x00
 
@@ -41,25 +35,23 @@ mainloop:
 
       mov [0x00], [0x00]
       ; update the screen coordinates based on the current x and y velocities:
-      sub [BALL_X], SPEED
       add [BALL_X], [BALL_VX]
-      sub [BALL_Y], SPEED
       add [BALL_Y], [BALL_VY]
 
       ; mirror the velocities if the symbol collides with the borders:
       jl [BALL_X], 320, _1
       mov [BALL_VX], 0
+      sub [BALL_VX], SPEED
 _1:
       jg [BALL_X], 0, _2
       mov [BALL_VX], SPEED
-      add [BALL_VX], SPEED
 _2:
       jl [BALL_Y], 200, _3
       mov [BALL_VY], 0
+      sub [BALL_VY], SPEED
 _3:
       jg [BALL_Y], 0, _4
       mov [BALL_VY], SPEED
-      add [BALL_VY], SPEED
 _4:
       jmp mainloop
 
