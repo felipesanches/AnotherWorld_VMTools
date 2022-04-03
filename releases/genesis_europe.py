501a27b9554091cfe3af802bc5aa14987f19bba5
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2022-04-03 15:26:01 -0300

    separate out data specific to individual game releases

---
+++ releases/genesis_europe.py
@@ -0,0 +1,32 @@
+from releases import common_data
+LABELED_CINEMATIC_ENTRIES = {
+  0: common_data.LABELED_CINEMATIC_ENTRIES.get(2, []),
+  1: common_data.LABELED_CINEMATIC_ENTRIES.get(3, []),
+  2: common_data.LABELED_CINEMATIC_ENTRIES.get(4, []),
+  3: common_data.LABELED_CINEMATIC_ENTRIES.get(5, []),
+  4: common_data.LABELED_CINEMATIC_ENTRIES.get(6, []),
+  5: common_data.LABELED_CINEMATIC_ENTRIES.get(7, []),
+  6: common_data.LABELED_CINEMATIC_ENTRIES.get(8, []),
+}
+
+POSSIBLY_UNUSED_CODEBLOCKS = {
+  0: [],
+
+  1: [],
+
+  2: [],
+
+  3: [],
+
+  4: [],
+
+  5: [],
+
+  6: []
+}
+
+KNOWN_LABELS = {
+  0: {
+  }
+}
+
